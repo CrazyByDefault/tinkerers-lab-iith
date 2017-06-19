ba36750c744c925ad21277d47abcea5486472bd8
CrazyByDefault <es16btech11025@iith.ac.in>
2017-06-19 22:54:16 +0530

    Add notifications badge

--- client/app.js
+++ client/app.js
@@ -58,21 +58,6 @@ angular.module('forum', ['angular-meteor', 'ui.router', 'angularTrix'])
             }
         });
 
-        const Images = new FilesCollection({
-            collectionName: 'Images',
-            allowClientCode: false, // Disallow remove files from Client
-            onBeforeUpload(file) {
-                // Allow upload files under 10MB, and only in png/jpg/jpeg formats
-                if (file.size <= 10485760 && /png|jpg|jpeg/i.test(file.extension)) {
-                    return true;
-                } else {
-                    return 'Please upload image, with size equal or less than 10MB';
-                }
-            }
-        });
-        
-        // $rootScope.subscribe('files.images.all');
-
         $rootScope.trustHtml = function(stuff) {
             return $sce.trustAsHtml(stuff);
         };
--- server/methods.js
+++ server/methods.js
@@ -1,72 +1,87 @@
 Meteor.methods({
-	createThread: function(topicId, title, content) {
-		check(topicId, String);
-		check(content, String);
+    createThread: function(topicId, title, content) {
+        check(topicId, String);
+        check(content, String);
 
-		var user = Meteor.user();
-		if(!user){
-			throw new Meteor.Error("You need to be logged in to do this!");
-		}
-		if(!title){
-			throw new Meteor.Error("Title can't be empty!")
-		}
-		if(!content){
-			throw new Meteor.Error("You can't create an empty thread!");
-		}
+        var user = Meteor.user();
+        if (!user) {
+            throw new Meteor.Error("You need to be logged in to do this!");
+        }
+        if (!title) {
+            throw new Meteor.Error("Title can't be empty!")
+        }
+        if (!content) {
+            throw new Meteor.Error("You can't create an empty thread!");
+        }
 
-		var thread = {
-			author: user.profile.name,
-			createdAt: new Date(),
-			topicId: topicId,
-			title: title,
-			content: content
-		};
-		return Threads.insert(thread);
-	},
+        var thread = {
+            author: user,
+            createdAt: new Date(),
+            topicId: topicId,
+            title: title,
+            content: content
+        };
+        return Threads.insert(thread);
+    },
 
-	createComment: function(blogId, content) {
-		check(blogId, String);
-		check(content, String);
+    createComment: function(blogId, content) {
+        check(blogId, String);
+        check(content, String);
 
-		var user = Meteor.user();
+        var user = Meteor.user();
 
-		if(!user){
-			throw new Meteor.Error("You need to be logged in to do this!");
-		}
-		if(!content){
-			throw new Meteor.Error("Comment can't be empty");
-		}
+        if (!user) {
+            throw new Meteor.Error("You need to be logged in to do this!");
+        }
+        if (!content) {
+            throw new Meteor.Error("Comment can't be empty");
+        }
 
-		var comment = {
-			author: user.profile.name,
-			createdAt: new Date(),
-			content: content
-		};
-		return Blogs.update({_id: blogId}, {
-			$addToSet: {comments: comment}
-		});
-	},
+        var comment = {
+            author: user,
+            createdAt: new Date(),
+            content: content
+        };
+        return Blogs.update({
+            _id: blogId
+        }, {
+            $addToSet: {
+                comments: comment
+            }
+        });
+    },
 
-	createReply: function(threadId, content) {
-		check(threadId, String);
-		check(content, String);
+    createReply: function(threadId, content) {
+        check(threadId, String);
+        check(content, String);
 
-		var user = Meteor.user();
+        var user = Meteor.user();
 
-		if(!user){
-			throw new Meteor.Error("You need to be logged in to do this!");
-		}
-		if(!content){
-			throw new Meteor.Error("Reply can't be empty");
-		}
+        if (!user) {
+            throw new Meteor.Error("You need to be logged in to do this!");
+        }
+        if (!content) {
+            throw new Meteor.Error("Reply can't be empty");
+        }
 
-		var reply = {
-			author: user.profile.name,
-			createdAt: new Date(),
-			content: content
-		};
-		return Threads.update({_id: threadId}, {
-			$addToSet: {replies: reply}
-		});
-	}
+        // Increment unread count for user who created post
+        Meteor.users.update({ _id: Threads.findOne({ _id: threadId }).author._id }, {
+        	$inc: {
+        		"profile.notifications": 1
+        	}
+        });
+
+        var reply = {
+            author: user.profile.name,
+            createdAt: new Date(),
+            content: content
+        };
+        return Threads.update({
+            _id: threadId
+        }, {
+            $addToSet: {
+                replies: reply
+            }
+        });
+    }
 });
