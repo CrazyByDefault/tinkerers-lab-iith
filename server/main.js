babe9abf4a3351ea9e5ac56fe7cd35961782a452
CrazyByDefault <es16btech11025@iith.ac.in>
2017-07-01 04:43:52 +0530

    Improve accessibility

--- server/main.js
+++ server/main.js
@@ -3,4 +3,7 @@ import { Accounts } from 'meteor/accounts-base';
 
 Meteor.startup(() => {
     // code to run on server at startup
+    Inject.rawModHtml("addLanguage", function(html) {
+        return html.replace(/<html>/, '<!-- HTML 5 -->\n<html lang="en">');
+      });
 });
