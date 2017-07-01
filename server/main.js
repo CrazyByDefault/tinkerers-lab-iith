fc08189539988f57fab9a79a067a45a44b67c8bc
CrazyByDefault <es16btech11025@iith.ac.in>
2017-07-01 15:12:36 +0530

    Add about us

--- server/main.js
+++ server/main.js
@@ -1,5 +1,6 @@
 import { Meteor } from 'meteor/meteor';
 import { Accounts } from 'meteor/accounts-base';
+import { Inject } from "meteor/meteorhacks:inject-initial";
 
 Meteor.startup(() => {
     // code to run on server at startup
